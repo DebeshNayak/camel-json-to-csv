2f83433ae184c3069ea5f8a8fe5569e97dc584f8
Debesh Nayak <dnayak382@gmail.com>
2022-01-03 20:54:43 +0530

    added file components to the router

--- src/main/java/com/debesh/jsontocsv/MySpringBootRouter.java
+++ src/main/java/com/debesh/jsontocsv/MySpringBootRouter.java
@@ -1,22 +1,26 @@
 package com.debesh.jsontocsv;
 
 import org.apache.camel.builder.RouteBuilder;
+import org.springframework.stereotype.Component;
 
 /**
  * A simple Camel route that triggers from a timer and calls a bean and print
  * @author dnaya
  *
  */
+@Component
 public class MySpringBootRouter extends RouteBuilder {
 
 	@Override
 	public void configure() throws Exception {
 		// Receive from JSON file 
-		from("")
+		from("file:files/input")
+				.log("Received file - ${body}")
 		// transform to CSV format 
 		
 		// Write to output file
-
+				.log("Writing file - ${body}")
+				.to("file:files/output");
 	}
 
 }
